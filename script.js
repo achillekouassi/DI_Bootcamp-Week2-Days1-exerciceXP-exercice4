1c32225e0a7082eceae8429848386a66e41e68a6
Achille kouassi <patriceachillekouassin@gmail.com>
2022-12-28 00:25:21 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let c;
+    let a = 34;
+    let b = 21;
+
+    console.log(a+b) //first expression
+    a = 2;
+    console.log(a+b) //second expression
+
+    /* 
+    1- le résultat de a + b dans la première expression est 55
+    2- le résultat de a + b dans la seconde expression est 23
+    3- la valeur de c est undefined
+    4- le résultat est 75
+    */
+ 
